9e7c99f397c0cd3eb7b506cc1032dd159995aaa5
RafaellaGuedes <rafaella.macedo@sptech.school>
2025-03-16 15:27:00 -0300

    Adição dos crud de empresa

---
+++ crud-h2/src/main/java/com/human_consulting/crud_h2/Empresa.java
@@ -0,0 +1,48 @@
+package com.human_consulting.crud_h2;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Empresa {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String nome;
+    private Usuario Diretor;
+    private String cnpj;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Usuario getDiretor() {
+        return Diretor;
+    }
+
+    public void setDiretor(Usuario diretor) {
+        Diretor = diretor;
+    }
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+}
